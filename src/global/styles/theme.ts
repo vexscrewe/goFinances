b35ab9572f1d3bf510dcceb16af671be594ffd00
vexscrewe <monokumaphu@gmail.com>
2025-04-25 22:08:52 -0400

    Criando componentes e add styled components

---
+++ src/global/styles/theme.ts
@@ -0,0 +1,29 @@
+export default {
+    colors: {
+        primary: '#000899',
+        secondary: '#8200CE',
+        background: '#f5f5f5',
+        error: '#b00200',
+        text: '#000000',
+    },
+
+    fonts: {
+        regular: 'Roboto-Regular',
+        medium: 'Roboto-Medium',
+        light: 'Roboto-Light',
+        thin: 'Roboto-Thin',
+    },
+
+    spacing: {
+        small: 8,
+        medium: 16,
+        large: 24,
+        xlarge: 32,
+    },
+    borderRadius: {
+        small: 4,
+        medium: 8,
+        large: 16,
+        xlarge: 32,
+    }
+}
---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,12 @@
+import { 
+    Container, 
+    Title 
+}   from "./styles";
+
+export function Dashboard(){
+    return (
+        <Container>
+            <Title>Dashboard</Title>
+        </Container>
+    );
+}
